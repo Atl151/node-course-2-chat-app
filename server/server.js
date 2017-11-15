3f73176124fee3915bb5923c9220f70170b8a828
Daniel Garcia <jdanielink@gmail.com>
2017-11-14 19:21:17 -0600

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Daniel',
+    text: 'Some text',
+    createdAt: 1342
+  });
+
+  socket.on('createMessage', (newMessage) => {
+    console.log('createMessage', newMessage);
+  })
+
   socket.on('disconnect', () => {
     console.log("Disconnected from client");
   });
