8c87603c91e801b0089f118b661cf95b0afa1f0e
Daniel Garcia <jdanielink@gmail.com>
2017-11-14 19:30:24 -0600

    Emit newMessage when createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Daniel',
-    text: 'Hey. It is Andrew'
-  });
 });
 
 socket.on('disconnect', function() {
